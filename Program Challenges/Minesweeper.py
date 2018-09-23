b11d0ab6f97bb32896f299133ad3c7fc0532544a
alan.jin <alan.jin@frozenmountain.com>
2018-09-23 08:46:29 -0700

    minesweeper accepted.

--- Program Challenges/Minesweeper.py
+++ Program Challenges/Minesweeper.py
@@ -13,35 +13,46 @@ class Mine:
 def caluclateMines(mineList):
     for index, mine in enumerate(mineList):
         for i, lineValue in enumerate(mine.data):
-            for j,value in enumerate(lineValue):
-                if(value == '.'):
+            for j,columnValue in enumerate(lineValue):
+                if(columnValue != '*'):
                     mineCount = 0
-                    if(i > 0 and mine.data[i-1][j] == '*'):
-                        mineCount = mineCount + 1
-                    if(i < mine.width - 1 and mine.data[i+1][j] == '*'):
-                        mineCount = mineCount + 1
+                    if(i > 0):
+                        if(mine.data[i-1][j] == '*'):
+                            mineCount = mineCount + 1
+                        if(j > 0 and mine.data[i-1][j-1] == '*'):
+                            mineCount = mineCount + 1
+                        if(j < mine.width-1 and mine.data[i-1][j+1] == '*'):
+                            mineCount = mineCount + 1
+                    if(i < mine.height - 1):
+                        if(mine.data[i+1][j] == '*'):
+                            mineCount = mineCount + 1
+                        if(j > 0 and mine.data[i+1][j-1] == '*'):
+                            mineCount = mineCount + 1
+                        if(j < mine.width-1 and mine.data[i+1][j+1] == '*'):
+                            mineCount = mineCount + 1
                     if(j > 0 and mine.data[i][j-1] == '*'):
                         mineCount = mineCount + 1
-                    if(j < mine.height - 1 and mine.data[i][j+1] == '*'):
+                    if(j < mine.width -1 and mine.data[i][j+1] == '*'):
                         mineCount = mineCount + 1
+                    
                     mine.data[i][j] = mineCount
-        print ("Field #",index + 1,":")
+        print ("Field #{}:".format(index+1))
         for lineValue in mine.data:
             print(''.join(str(i) for i in lineValue))
-        print('\n')
-    os._exit()
-
+        if(index < len(mineList) - 1):
+            print()
 if __name__ == '__main__':
     currentMine = Mine()
     mineList = []        
     for line in sys.stdin:
         characters = line.split()
         if len(characters) == 2 and all(str.isnumeric(i) for i in characters):
-            width, height = map(int,characters[:2])
+            height, width = map(int,characters[:2])
             if(width == 0 and height == 0):
                 if currentMine.width > 0 and currentMine.height > 0:
                     mineList.append(currentMine)
                 caluclateMines(mineList)
+                break
             elif currentMine.width > 0 and currentMine.height > 0:
                 mineList.append(currentMine)
                 currentMine = Mine()
